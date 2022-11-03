3495e4441d0fd9e5e08d7ee5b30c710289609fca
miaoyin <miaoyin@woda.com>
2022-11-03 14:00:09 +0800

    fix: 滑动窗口限频计时器失效错误

--- syncs/rdb/limit.go
+++ syncs/rdb/limit.go
@@ -10,26 +10,31 @@ import (
 	"github.com/xuender/oils/syncs"
 )
 
-const (
-	_windowNum  = 10
-	_expiration = time.Second / _windowNum
-)
+const _defaultWindow = 10
 
 // Limit 滑动窗口限流.
 type Limit struct {
-	key      string
-	keyTimer string
-	qps      int64
-	client   *redis.Client
+	key        string
+	keyTimer   string
+	qps        int64
+	client     *redis.Client
+	expiration time.Duration
 }
 
 // NewLimit 频率限制, client redis客户端, key 键，qps 每秒限制次数.
 func NewLimit(client *redis.Client, key string, qps uint) *Limit {
+	windowNum := _defaultWindow
+
+	if qps < _defaultWindow {
+		windowNum = int(qps)
+	}
+
 	ret := &Limit{
-		client:   client,
-		key:      key,
-		keyTimer: fmt.Sprintf("_%s_", key),
-		qps:      int64(qps / _windowNum),
+		client:     client,
+		key:        key,
+		keyTimer:   fmt.Sprintf("_%s_", key),
+		qps:        int64(qps) / int64(windowNum),
+		expiration: time.Second / time.Duration(windowNum),
 	}
 
 	return ret
@@ -45,16 +50,18 @@ func (p *Limit) Wait() {
 func (p *Limit) waiting() time.Duration {
 	ctx := context.Background()
 	// 设置计时器
-	if base.Must1(p.client.SetNX(ctx, p.keyTimer, "1", _expiration).Result()) {
+	if base.Must1(p.client.SetNX(ctx, p.keyTimer, "1", p.expiration).Result()) {
 		// 计时器设置成功后删除计数器
 		base.Must1(p.client.Del(ctx, p.key).Result())
 	}
 	// 计数器大与QPS
 	if base.Must1(p.client.Incr(ctx, p.key).Result()) > p.qps {
 		// 获取计时器剩余时间
-		if dur := base.Must1(p.client.PTTL(ctx, p.keyTimer).Result()); dur > 0 {
+		if dur := base.Must1(p.client.PTTL(ctx, p.keyTimer).Result()); dur >= 0 {
 			return dur
 		}
+		// 延时重试
+		return p.expiration / _defaultWindow
 	}
 
 	return 0
