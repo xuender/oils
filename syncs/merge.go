0015c36cc04fd2bbc896d707c6f5f1fed0357ccd
miaoyin <miaoyin@woda.com>
2022-11-08 13:03:39 +0800

    feat: 多管道合并

---
+++ syncs/merge.go
@@ -0,0 +1,43 @@
+package syncs
+
+// Merge 多个管道内容合并.
+func Merge[T any](less func(T, T) bool, chans ...<-chan T) <-chan T {
+	out := make(chan T)
+	opens := map[int]T{}
+
+	go func() {
+		for i, cha := range chans {
+			if value, open := <-cha; open {
+				opens[i] = value
+			}
+		}
+
+		var (
+			minIndex int
+			min      T
+		)
+
+		for len(opens) > 0 {
+			minIndex = -1
+
+			for i, value := range opens {
+				if minIndex == -1 || less(value, min) {
+					minIndex = i
+					min = value
+				}
+			}
+
+			out <- min
+
+			if value, open := <-chans[minIndex]; open {
+				opens[minIndex] = value
+			} else {
+				delete(opens, minIndex)
+			}
+		}
+
+		close(out)
+	}()
+
+	return out
+}
---
+++ syncs/merge_test.go
@@ -0,0 +1,56 @@
+package syncs_test
+
+import (
+	"testing"
+
+	"github.com/xuender/oils/assert"
+	"github.com/xuender/oils/syncs"
+)
+
+func TestMerge(t *testing.T) {
+	t.Parallel()
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+
+	out := syncs.Merge(func(int1, int2 int) bool {
+		return int1 < int2
+	}, ch1, ch2, ch3)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			if i%2 == 0 {
+				ch2 <- i
+			}
+		}
+		close(ch2)
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			if i%3 == 0 {
+				ch1 <- i
+			}
+		}
+		close(ch1)
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			if i%5 == 0 {
+				ch3 <- i
+			}
+		}
+		close(ch3)
+	}()
+
+	res := []int{}
+	for value := range out {
+		res = append(res, value)
+	}
+
+	assert.Equal(t, 11, len(res))
+	assert.Equal(t, 0, res[0])
+	assert.Equal(t, 9, res[10])
+}
