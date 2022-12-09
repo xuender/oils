26084303cbe9a6c7bc9b5e45c601b5275c9a40b5
miaoyin <miaoyin@woda.com>
2022-12-09 16:50:28 +0800

    feat: base add const

--- base/const.go
+++ base/const.go
@@ -1,19 +1,28 @@
 package base
 
 const (
-	Two            = 2
 	There          = 3
-	Four           = 4
 	Five           = 5
 	Six            = 6
 	Seven          = 7
-	Eight          = 8
 	Nine           = 9
 	Ten            = 10
-	Sixteen        = 16
-	SixtyFour      = 64
 	Hundred        = 100
 	Million        = 1e6
 	HundredMillion = 1e8
 	Billion        = 1e9
+)
+
+const (
+	One = 1 << iota
+	Two
+	Four
+	Eight
+	Sixteen
+	ThirtyTwo
+	SixtyFour
+	OneHundredTwentyEight
+	TwoHundredFiftySix
+	FiveHundredTwelve
+	Kilo
 )
---
+++ base/example_const_test.go
@@ -0,0 +1,28 @@
+package base_test
+
+import (
+	"fmt"
+
+	"github.com/xuender/oils/base"
+)
+
+func ExampleThere() {
+	fmt.Println(base.One)
+	fmt.Println(base.Two)
+	fmt.Println(base.There)
+	fmt.Println(base.Four)
+	fmt.Println(base.Five)
+	fmt.Println(base.SixtyFour)
+	fmt.Println(base.OneHundredTwentyEight)
+	fmt.Println(base.Kilo)
+
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 64
+	// 128
+	// 1024
+}
